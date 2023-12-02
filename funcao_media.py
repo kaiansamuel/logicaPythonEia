711e4fffdf6a6adf4a512697ae8f19aca378767f
Kaian Samuel <kaiansamuelster@gmail.com>
2023-12-02 20:54:01 -0300

    Criação do repositorio e primeiro exercicio

---
+++ funcao_media.py
@@ -0,0 +1,7 @@
+def media(a, b):
+    return (a + b) / 2
+
+val1 = int(input('Informe o primeiro valor:'))
+val2 = int(input('Informe o segundo valor:'))
+resultado = media(val1, val2)
+print('O resultado é: ', resultado)
